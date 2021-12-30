988b945c270bbb53aced9202e593a6264a801cb7
Iury Brasileiro <iury.brasileiro@outlook.com.br>
2021-12-30 00:02:52 -0300

    feat: add window metrics

--- src/theme/metrics.ts
+++ src/theme/metrics.ts
@@ -1,10 +1,16 @@
-import { Platform } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 import {
   getBottomSpace,
   getStatusBarHeight,
 } from 'react-native-iphone-x-helper';
 
+const { width: WIDTH, height: HEIGHT } = Dimensions.get('window');
+
 export default {
+  window: {
+    width: WIDTH,
+    height: HEIGHT,
+  },
   statusBarHeight: Platform.select({
     ios: getStatusBarHeight(true),
     android: 0,
