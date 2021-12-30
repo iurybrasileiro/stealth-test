3e1a9cb301151d0d8b85920a88fa62854da809af
Iury Brasileiro <iury.brasileiro@outlook.com.br>
2021-12-29 22:21:33 -0300

    feat: add font in the theme

---
+++ src/theme/font.ts
@@ -0,0 +1,5 @@
+import { fontSizePercentage } from '~/utils/size';
+
+export default {
+  size: (size: number) => fontSizePercentage(size),
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,9 @@
 import colors from './colors';
+import font from './font';
 import metrics from './metrics';
 
 export default {
   colors,
-
+  font,
   metrics,
 };
