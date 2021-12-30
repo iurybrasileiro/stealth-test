4a17b0ace0cb63e4d9ec917915e0a02d596c1203
Iury Brasileiro <iury.brasileiro@outlook.com.br>
2021-12-29 21:55:13 -0300

    feat: configure theme with global colors

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './theme';
+
 function Main() {
-  return <View />;
+  return (
+    <ThemeProvider theme={theme}>
+      <View />
+    </ThemeProvider>
+  );
 }
 
 export default Main;
---
+++ src/theme/colors.ts
@@ -0,0 +1,8 @@
+const global = {
+  primary: '#169be5',
+  secondary: '#0f6b9e',
+};
+
+export default {
+  global,
+};
