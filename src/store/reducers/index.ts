f9aa1a4f16975a9b73512451a03924c9ec1969b3
Iury Brasileiro <iury.brasileiro@outlook.com.br>
2022-01-03 23:40:21 -0300

    feat: add translations reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import questions from './questions';
+import translations from './translations';
 
 export default combineReducers({
   questions,
+  translations,
 });
